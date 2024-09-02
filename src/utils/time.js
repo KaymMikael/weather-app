a6376ea517d8c57727a83b80c1783f33e0e56d5e
Sero <kmcaltiz@gmail.com>
2024-09-02 18:09:18 +0800

    add util time.js

---
+++ src/utils/time.js
@@ -0,0 +1,15 @@
+const convertUnixToTime = (unixTimeStamp) => {
+    const milliSeconds = unixTimeStamp * 1000; //convert to milliseconds
+    const date = new Date(milliSeconds);
+    
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+
+    hours = hours % 12 || 12;
+    const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
+    
+    return `${hours}:${formattedMinutes} ${ampm}`;
+}
+
+console.log(convertUnixToTime(1725271609));
