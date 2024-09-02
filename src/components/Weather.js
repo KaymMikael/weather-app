c5ec37d49e5a223437bcd179f45238353619d97c
Sero <kmcaltiz@gmail.com>
2024-09-02 18:09:52 +0800

    add weather degrees and wind

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -8,9 +8,21 @@ const Weather = () => {
       </header>
       <section className="weather-data">
         <div className="weather-condition">
-            <img src="https://openweathermap.org/img/wn/10d.png" alt="Weather Condition" className="weather-condition-icon"/>
+            <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="Weather Condition" className="weather-condition-icon"/>
             <p>Rain</p>
         </div>
+        <div className="weather-degrees">
+            <p>21.0 &deg;c</p>
+            <p>Minimum: 21.0 &deg;c</p>
+            <p>Maximum: 21.0 &deg;c</p>
+        </div>
+        <div className="weather-wind">
+            <p>Wind speed: 0.62 ms</p>
+            <p>Wind direction: 349</p>
+        </div>
+      </section>
+      <section className="sun-data">
+        
       </section>
     </section>
   );
